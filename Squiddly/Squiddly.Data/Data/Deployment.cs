c1bdc8b4667307195d2c5a94861b3cc63b72f9a5
Steve Hoff <smhoff256@gmail.com>
2019-04-11 11:53:29 -0500

    Setting all the project to netcore 2.2

---
+++ Squiddly/Squiddly.Data/Data/Deployment.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Squiddly.Data.Data
+{
+    public class Deployment
+    {
+        [Key]
+        public int DeploymentId { get; set; }
+
+        public string DeploymentName { get; set; }
+    }
+}
---
+++ Squiddly/Squiddly.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Squiddly.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
